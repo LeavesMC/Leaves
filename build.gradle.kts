cfa7dd3724bb1f6ea87d876d207795c83e382d15
violetc <58360096+s-yh-china@users.noreply.github.com>
2022-04-09 14:23:41 +0800

    Update Paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.1:fat")
-    decompiler("net.minecraftforge:forgeflower:1.5.498.22")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.29")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 

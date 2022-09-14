de9a5269fc276de5e44176a517fe427380ae5151
violetc <admin@s-yh-china.com>
2022-09-14 09:18:42 +0000

    Fix text error

--- scripts/GetReleaseInfo.sh
+++ scripts/GetReleaseInfo.sh
@@ -6,15 +6,16 @@ md5() {
   md5sum $1 | awk '{print $1}'
 }
 
-tagid="1.19.2-`git log --pretty='%h' -1`"
+commitid=`git log --pretty='%h' -1`
+tagid="1.19.2-$commitid"
 
-echo "Leaves-$tagid [![download](https://img.shields.io/github/downloads/LeavesMC/Leaves/$tagid/total?color=0)](https://github.com/Leaves/LeavesMC/releases/download/$tagid/leaves-1.19.2.jar)" >> releaseinfo.md
+echo "Leaves-$commitid [![download](https://img.shields.io/github/downloads/LeavesMC/Leaves/$tagid/total?color=0)](https://github.com/Leaves/LeavesMC/releases/download/$tagid/leaves-1.19.2.jar)" >> releaseinfo.md
 echo "=====" >> releaseinfo.md
 echo "" >> releaseinfo.md
 echo "### Commit Message" >> releaseinfo.md
 
 number=$(git log --oneline master ^`git describe --tags --abbrev=0` | wc -l)
-echo `git log --pretty='> [%h] %s' -$number` >> releaseinfo.md
+echo "$(git log --pretty='> [%h] %s' -$number)" >> releaseinfo.md
 
 echo "" >> releaseinfo.md
 echo "### Checksum" >> releaseinfo.md

4c5e012d0136bd8c9e0b18beccbf1f41405e7f6f
violetc <58360096+s-yh-china@users.noreply.github.com>
2022-09-16 08:09:00 +0000

    [ci skip] read gradle.properties to create release

--- scripts/GetReleaseInfo.sh
+++ scripts/GetReleaseInfo.sh
@@ -6,20 +6,36 @@ md5() {
   md5sum $1 | awk '{print $1}'
 }
 
-commitid=`git log --pretty='%h' -1`
-tagid="1.19.2-$commitid"
+prop() {
+  grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
+}
 
-echo "Leaves-$commitid [![download](https://img.shields.io/github/downloads/LeavesMC/Leaves/$tagid/total?color=0)](https://github.com/Leaves/LeavesMC/releases/download/$tagid/leaves-1.19.2.jar)" >> releaseinfo.md
-echo "=====" >> releaseinfo.md
-echo "" >> releaseinfo.md
-echo "### Commit Message" >> releaseinfo.md
+commitid=$(git log --pretty='%h' -1)
+mcversion=$(prop mcVersion)
+gradleVersion=$(prop version)
+tagid="$mcversion-$commitid"
+jarName="leaves-$mcversion.jar"
+releaseinfo="releaseinfo.md"
+
+rm -f $releaseinfo
+
+mv build/libs/Leaves-paperclip-$gradleVersion-reobf.jar $jarName
+echo "name=Leaves-$commitid" >> $GITHUB_ENV
+echo "tag=$tagid" >> $GITHUB_ENV
+echo "jar=$jarName" >> $GITHUB_ENV
+echo "info=$releaseinfo" >> $GITHUB_ENV
+
+echo "Leaves-$commitid [![download](https://img.shields.io/github/downloads/LeavesMC/Leaves/$tagid/total?color=0)](https://github.com/Leaves/LeavesMC/releases/download/$tagid/$jarName)" >> $releaseinfo
+echo "=====" >> $releaseinfo
+echo "" >> $releaseinfo
+echo "### Commit Message" >> $releaseinfo
 
 number=$(git log --oneline master ^`git describe --tags --abbrev=0` | wc -l)
-echo "$(git log --pretty='> [%h] %s' -$number)" >> releaseinfo.md
+echo "$(git log --pretty='> [%h] %s' -$number)" >> $releaseinfo
 
-echo "" >> releaseinfo.md
-echo "### Checksum" >> releaseinfo.md
-echo "| File | leaves-1.19.2.jar |" >> releaseinfo.md
-echo "| ---- | ---- |" >> releaseinfo.md
-echo "| MD5 | `md5 "leaves-1.19.2.jar"` |" >> releaseinfo.md
-echo "| SHA1 | `sha1 "leaves-1.19.2.jar"` |" >> releaseinfo.md
+echo "" >> $releaseinfo
+echo "### Checksum" >> $releaseinfo
+echo "| File | $jarName |" >> $releaseinfo
+echo "| ---- | ---- |" >> $releaseinfo
+echo "| MD5 | `md5 $jarName` |" >> $releaseinfo
+echo "| SHA1 | `sha1 $jarName` |" >> $releaseinfo

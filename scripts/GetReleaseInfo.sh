e042e85baac1d47551cf46d07c294d302fa5a719
violetc <58360096+s-yh-china@users.noreply.github.com>
2022-11-29 20:51:18 +0800

    Update CI

--- scripts/GetReleaseInfo.sh
+++ scripts/GetReleaseInfo.sh
@@ -15,25 +15,27 @@ mcversion=$(prop mcVersion)
 gradleVersion=$(prop version)
 tagid="$mcversion-$commitid"
 jarName="leaves-$mcversion.jar"
+leavesid="Leaves-$commitid"
 releaseinfo="releaseinfo.md"
+discordmes="discordmes.json"
 
+rm -f $discordmes
 rm -f $releaseinfo
 
 mv build/libs/Leaves-paperclip-$gradleVersion-reobf.jar $jarName
-echo "name=Leaves-$commitid" >> $GITHUB_ENV
+echo "name=$leavesid" >> $GITHUB_ENV
 echo "tag=$tagid" >> $GITHUB_ENV
 echo "jar=$jarName" >> $GITHUB_ENV
 echo "info=$releaseinfo" >> $GITHUB_ENV
-echo "url=https://github.com/LeavesMC/Leaves/releases/tag/$tagid" >> $GITHUB_ENV
+echo "discordmes=$discordmes" >> $GITHUB_ENV
 
-echo "Leaves-$commitid [![download](https://img.shields.io/github/downloads/LeavesMC/Leaves/$tagid/total?color=0)](https://github.com/Leaves/LeavesMC/releases/download/$tagid/$jarName)" >> $releaseinfo
+echo "$leavesid [![download](https://img.shields.io/github/downloads/LeavesMC/Leaves/$tagid/total?color=0)](https://github.com/Leaves/LeavesMC/releases/download/$tagid/$jarName)" >> $releaseinfo
 echo "=====" >> $releaseinfo
 echo "" >> $releaseinfo
 echo "### Commit Message" >> $releaseinfo
 
 number=$(git log --oneline master ^`git describe --tags --abbrev=0` | wc -l)
 echo "$(git log --pretty='> [%h] %s' -$number)" >> $releaseinfo
-echo "changelog=$(git log --pretty='> [%h] %s' -$number)" >> $GITHUB_ENV
 
 echo "" >> $releaseinfo
 echo "### Checksum" >> $releaseinfo
@@ -41,3 +43,6 @@ echo "| File | $jarName |" >> $releaseinfo
 echo "| ---- | ---- |" >> $releaseinfo
 echo "| MD5 | `md5 $jarName` |" >> $releaseinfo
 echo "| SHA1 | `sha1 $jarName` |" >> $releaseinfo
+
+echo "{\"embeds\":[{\"title\":\"$leavesid\",\"url\":\"https://github.com/LeavesMC/Leaves/releases/tag/$tagid\",\"fields\":[{\"name\":\"Changelog\",\"value\":" >> $discordmes
+echo "\"$(git log --pretty='> [%h] %s' -$number)\",\"inline\":true}]}]}" >> $discordmes

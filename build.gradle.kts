ff82c70b4cb8dcf3e829c399c621db8f840aae14
violetc <58360096+s-yh-china@users.noreply.github.com>
2023-03-05 16:23:40 +0800

    Update Paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.1"
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.2"
 }
 
 repositories {

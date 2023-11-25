d7737eb4b5c18cb4962160d66282a9ae0da3b158
violetc <58360096+s-yh-china@users.noreply.github.com>
2023-11-25 11:42:41 +0800

    Update Paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.8"
+    id("io.papermc.paperweight.patcher") version "1.5.10"
 }
 
 repositories {

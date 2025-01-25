00798036278e44bfafc0e918d7e90db0145001c4
violetc <58360096+s-yh-china@users.noreply.github.com>
2025-01-25 16:51:52 +0800

    init 1.21.4, and boom!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,45 @@
 plugins {
     java
-    `maven-publish`
-    id("org.leavesmc.leavesweight.patcher") version "1.0.2-SNAPSHOT"
+    id("org.leavesmc.leavesweight.patcher") version "2.0.0-SNAPSHOT"
 }
 
-allprojects {
-    apply(plugin = "java")
-    apply(plugin = "maven-publish")
+paperweight {
+    upstreams.paper {
+        ref = providers.gradleProperty("paperRef")
 
-    java {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(21)
+        patchFile {
+            path = "paper-server/build.gradle.kts"
+            outputFile = file("leaves-server/build.gradle.kts")
+            patchFile = file("leaves-server/build.gradle.kts.patch")
+        }
+        patchFile {
+            path = "paper-api/build.gradle.kts"
+            outputFile = file("leaves-api/build.gradle.kts")
+            patchFile = file("leaves-api/build.gradle.kts.patch")
+        }
+        patchDir("paperApi") {
+            upstreamPath = "paper-api"
+            excludes = setOf("build.gradle.kts")
+            patchesDir = file("leaves-api/paper-patches")
+            outputDir = file("leaves-api")
         }
     }
 }
 
 subprojects {
-    apply(plugin = "java")
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
 
-    java {
+    extensions.configure<JavaPluginExtension> {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21))
+            languageVersion = JavaLanguageVersion.of(21)
         }
     }
 
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
     tasks.withType<JavaCompile> {
         options.encoding = Charsets.UTF_8.name()
         options.release.set(21)
@@ -38,55 +54,13 @@ subprojects {
     repositories {
         mavenLocal()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/groups/public/")
         maven("https://repo.papermc.io/repository/maven-public/")
-        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
-        maven("https://repo.aikar.co/content/groups/aikar")
-        maven("https://repo.md-5.net/content/repositories/releases/")
-        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
-        maven("https://jitpack.io")
-    }
-}
-
-repositories {
-    mavenCentral()
-    maven("https://repo.leavesmc.org/releases") {
-        content { onlyForConfigurations("leavesclip") }
-    }
-}
-
-dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.10.3:fat")
-    decompiler("org.vineflower:vineflower:1.10.1")
-    leavesclip("org.leavesmc:leavesclip:2.0.1")
-}
-
-paperweight {
-    serverProject.set(project(":leaves-server"))
-
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
-
-    usePaperArchiveUpstream(providers.gradleProperty("paperRef")) {
-        withPaperPatcher {
-            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
-            apiOutputDir.set(layout.projectDirectory.dir("leaves-api"))
-
-            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
-            serverOutputDir.set(layout.projectDirectory.dir("leaves-server"))
-        }
-
-        patchTasks.register("generatedApi") {
-            isBareDirectory = true
-            upstreamDirPath = "paper-api-generator/generated"
-            patchDir = layout.projectDirectory.dir("patches/generated-api")
-            outputDir = layout.projectDirectory.dir("paper-api-generator/generated")
+        maven("https://repo.leavesmc.org/releases") {
+            content { onlyForConfigurations("leavesclip") }
         }
     }
-}
 
-allprojects {
-    publishing {
+    extensions.configure<PublishingExtension> {
         repositories {
             maven("https://repo.leavesmc.org/snapshots") {
                 name = "leaves"

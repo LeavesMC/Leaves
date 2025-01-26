05ee2e369cf1e094fd624e3d7d9ba00d88b58985
violetc <58360096+s-yh-china@users.noreply.github.com>
2025-01-26 14:58:11 +0800

    Build changes, and delete timings

---
+++ leaves-server/src/main/java/org/leavesmc/leaves/region/LeavesHooks.java
@@ -0,0 +1,19 @@
+package org.leavesmc.leaves.region;
+
+import ca.spottedleaf.moonrise.paper.PaperHooks;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.chunk.LevelChunk;
+
+public final class LeavesHooks extends PaperHooks {
+
+    @Override
+    public String getBrand() {
+        return "Leaves";
+    }
+
+    @Override
+    public void onChunkWatch(ServerLevel world, LevelChunk chunk, ServerPlayer player) {
+        super.onChunkWatch(world, chunk, player);
+    }
+}

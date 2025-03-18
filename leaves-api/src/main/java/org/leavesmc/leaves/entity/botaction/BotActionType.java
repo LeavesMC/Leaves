35ff0b5816e898397cd9e6d123fe9bedee83e2c9
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-03-18 21:02:47 +0800

    Great!

--- leaves-api/src/main/java/org/leavesmc/leaves/entity/botaction/BotActionType.java
+++ leaves-api/src/main/java/org/leavesmc/leaves/entity/botaction/BotActionType.java
@@ -13,8 +13,6 @@ public enum BotActionType {
     ROTATE("rotate"),
     ROTATION("rotation"),
     SNEAK("sneak"),
-    @Deprecated(forRemoval = true, since = "1.21.4")
-    STOP("stop"),
     SWIM("swim"),
     USE("use"),
     USE_ON("use_on"),

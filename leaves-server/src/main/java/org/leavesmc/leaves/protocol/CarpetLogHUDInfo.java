8811d571393c2c896caec097013fe421a68a78fa
Wzp-2008 <minecraftwzpmc@163.com>
2025-03-27 16:20:23 +0800

    fix: make it compact with other plugin

--- leaves-server/src/main/java/org/leavesmc/leaves/protocol/CarpetLogHUDInfo.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/protocol/CarpetLogHUDInfo.java
@@ -1,8 +1,11 @@
 package org.leavesmc.leaves.protocol;
 
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.protocol.game.ClientboundTabListPacket;
 import net.minecraft.server.MinecraftServer;
@@ -12,12 +15,14 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.TimeUtil;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.NaturalSpawner;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.leavesmc.leaves.LeavesConfig;
 import org.leavesmc.leaves.protocol.core.LeavesProtocol;
 import org.leavesmc.leaves.protocol.core.ProtocolHandler;
 import oshi.util.tuples.Pair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -28,23 +33,25 @@ public class CarpetLogHUDInfo {
 
     private static MobCategory[] categories;
 
-    public static ChatFormatting creatureTypeColor(MobCategory type) {
+    private static Map<CraftPlayer, Component> playersMessage = new HashMap<>();
+
+    public static NamedTextColor creatureTypeColor(MobCategory type) {
         return switch (type) {
-            case MONSTER -> ChatFormatting.DARK_RED;
-            case CREATURE -> ChatFormatting.DARK_GREEN;
-            case AMBIENT -> ChatFormatting.DARK_GRAY;
-            case WATER_CREATURE -> ChatFormatting.DARK_BLUE;
-            case WATER_AMBIENT -> ChatFormatting.DARK_AQUA;
-            default -> ChatFormatting.WHITE;
+            case MONSTER -> NamedTextColor.DARK_RED;
+            case CREATURE -> NamedTextColor.DARK_GREEN;
+            case AMBIENT -> NamedTextColor.DARK_GRAY;
+            case WATER_CREATURE -> NamedTextColor.DARK_BLUE;
+            case WATER_AMBIENT -> NamedTextColor.DARK_AQUA;
+            default -> NamedTextColor.WHITE;
         };
     }
 
-    public static ChatFormatting heatmapColor(double actual, double reference) {
-        ChatFormatting color = ChatFormatting.GRAY;
-        if (actual >= 0.0D) color = ChatFormatting.DARK_GREEN;
-        if (actual > 0.5D * reference) color = ChatFormatting.YELLOW;
-        if (actual > 0.8D * reference) color = ChatFormatting.RED;
-        if (actual > reference) color = ChatFormatting.LIGHT_PURPLE;
+    public static TextColor heatmapColor(double actual, double reference) {
+        NamedTextColor color = NamedTextColor.GRAY;
+        if (actual >= 0.0D) color = NamedTextColor.DARK_GREEN;
+        if (actual > 0.5D * reference) color = NamedTextColor.YELLOW;
+        if (actual > 0.8D * reference) color = NamedTextColor.RED;
+        if (actual > reference) color = NamedTextColor.LIGHT_PURPLE;
         return color;
     }
 
@@ -66,51 +73,50 @@ public class CarpetLogHUDInfo {
             double mspt = (double) server.getAverageTickTimeNanos() / TimeUtil.NANOSECONDS_PER_MILLISECOND;
             float msptMax = tickRateManager.millisecondsPerTick();
             double tps = 1000.0D / Math.max(tickRateManager.isSprinting() ? 0 : msptMax, mspt);
-            ChatFormatting color = heatmapColor(mspt, msptMax);
-            MutableComponent message = Component.empty()
+            TextColor color = heatmapColor(mspt, msptMax);
+            Component message = Component.empty()
                 .append(
-                    Component.literal("TPS: ").withStyle(ChatFormatting.GRAY)
+                    Component.text("TPS: ").color(NamedTextColor.GRAY)
                 ).append(
-                    Component.literal(String.format("%.1f", tps)).withStyle(color)
+                    Component.text(String.format("%.1f", tps)).color(color)
                 ).append(
-                    Component.literal(" MSPT: ").withStyle(ChatFormatting.GRAY)
+                    Component.text(" MSPT: ").color(NamedTextColor.GRAY)
                 ).append(
-                    Component.literal(String.format("%.1f", mspt)).withStyle(color)
+                    Component.text(String.format("%.1f", mspt)).color(color)
                 );
 
             // Send MobCaps Data
 
             Set<ServerLevel> levels = realPlayers.stream().map(ServerPlayer::serverLevel).collect(Collectors.toUnmodifiableSet());
-            Map<ServerLevel, MutableComponent> serverLevelEntitiesMessageMap = levels.stream().map(world -> {
+            Map<ServerLevel, Component> serverLevelEntitiesMessageMap = levels.stream().map(world -> {
                 List<Component> lst = new ArrayList<>();
                 NaturalSpawner.SpawnState lastSpawner = world.getChunkSource().getLastSpawnState();
                 int chunkcount = lastSpawner == null ? 0 : lastSpawner.getSpawnableChunkCount();
                 Object2IntMap<MobCategory> dimCounts = lastSpawner == null ? null : lastSpawner.getMobCategoryCounts();
                 if (dimCounts == null || chunkcount < 0) {
-                    lst.add(Component.literal("  --UNAVAILABLE--").withStyle(ChatFormatting.GRAY));
+                    lst.add(Component.text("  --UNAVAILABLE--").color(NamedTextColor.GRAY));
                     return new Pair<>(world, lst);
                 }
                 List<Component> shortCodes = new ArrayList<>();
                 for (MobCategory category : categories) {
                     int cur = lastSpawner.getMobCategoryCounts().getOrDefault(category, 0);
                     int max = NaturalSpawner.globalLimitForCategory(world, category, chunkcount);
-                    ChatFormatting mobColor = creatureTypeColor(category);
-                    shortCodes.add(Component.literal("" + (cur < 0 ? "-" : cur)).withStyle(heatmapColor(cur, max)));
-                    shortCodes.add(Component.literal("/").withStyle(ChatFormatting.GRAY));
-                    shortCodes.add(Component.literal(String.valueOf(max)).withStyle(mobColor));
-                    shortCodes.add(Component.literal(",").withStyle(ChatFormatting.GRAY));
+                    TextColor mobColor = creatureTypeColor(category);
+                    shortCodes.add(Component.text("" + (cur < 0 ? "-" : cur)).color(heatmapColor(cur, max)));
+                    shortCodes.add(Component.text("/").color(NamedTextColor.GRAY));
+                    shortCodes.add(Component.text(String.valueOf(max)).color(mobColor));
+                    shortCodes.add(Component.text(",").color(NamedTextColor.GRAY));
                 }
                 if (!shortCodes.isEmpty()) {
                     shortCodes.removeLast();
                     lst.addAll(shortCodes);
                 } else {
-                    lst.add(Component.literal("  --UNAVAILABLE--").withStyle(ChatFormatting.GRAY));
+                    lst.add(Component.text("  --UNAVAILABLE--").color(NamedTextColor.GRAY));
                 }
                 return new Pair<>(world, lst);
             }).map(e -> {
                 List<Component> b = e.getB();
-                MutableComponent full = Component.empty();
-                b.forEach(full::append);
+                Component full = Component.join(JoinConfiguration.builder().build(), b);
                 return new Pair<>(e.getA(), full);
             }).collect(Collectors.toMap(Pair::getA, Pair::getB));
 
@@ -118,10 +124,16 @@ public class CarpetLogHUDInfo {
             // send data to tab bar
             realPlayers.forEach(player -> {
                 ServerLevel serverLevel = player.serverLevel();
-                MutableComponent components = serverLevelEntitiesMessageMap.get(serverLevel);
-                ClientboundTabListPacket packet = new ClientboundTabListPacket(Component.empty(), Component.empty().append(message).append(Component.literal("\n")).append(components));
-                player.connection.send(packet);
+                Component components = serverLevelEntitiesMessageMap.get(serverLevel);
+                CraftPlayer bukkitEntity = player.getBukkitEntity();
+                Component currentPlayerData = Component.join(JoinConfiguration.newlines(), message, components);
+                playersMessage.put(bukkitEntity, currentPlayerData);
+                bukkitEntity.leaves$refreshPlayerListHeaderAndFooter();
             });
         }
+    }
+
+    public static Component getPlayerMessage(CraftPlayer player) {
+        return playersMessage.get(player);
     }
 }

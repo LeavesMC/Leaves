fd1397765df66af27dd003332b154270fb47970a
MC_XiaoHei <xor7xiaohei@gmail.com>
2025-05-04 12:55:46 +0000

    feat: update leaves plugin

---
+++ leaves-server/src/main/java/org/leavesmc/leaves/plugin/ServerFeatureManager.java
@@ -0,0 +1,29 @@
+package org.leavesmc.leaves.plugin;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.leavesmc.leaves.plugin.Features.*;
+
+public class ServerFeatureManager implements FeatureManager {
+    public static ServerFeatureManager INSTANCE = new ServerFeatureManager();
+    private final Set<String> availableFeatures = new HashSet<>();
+
+    private ServerFeatureManager() {
+        availableFeatures.addAll(Set.of(
+            MIXIN,
+            FAKEPLAYER,
+            PHOTOGRAPHER
+        ));
+    }
+
+    @Override
+    public Set<String> getAvailableFeatures() {
+        return availableFeatures;
+    }
+
+    @Override
+    public boolean isFeatureAvailable(String feature) {
+        return availableFeatures.contains(feature);
+    }
+}

10e92761e5ccd21d835c607692c693d6e7cbc903
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-05-17 07:37:06 -0400

    Stupid me

--- leaves-server/src/main/java/org/leavesmc/leaves/util/UpdateSuppressionError.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/util/UpdateSuppressionError.java
@@ -3,12 +3,12 @@ package org.leavesmc.leaves.util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
 
-public class UpdateSuppressionException extends RuntimeException {
+public class UpdateSuppressionError extends Error {
 
     private final BlockPos pos;
     private final Block source;
 
-    public UpdateSuppressionException(BlockPos pos, Block source) {
+    public UpdateSuppressionError(BlockPos pos, Block source) {
         super("Update suppression");
         this.pos = pos;
         this.source = source;

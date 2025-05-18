6678e74f05c18ba7cf3c6c1071535c53394073fb
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-05-18 23:49:59 +0800

    feat: old nether portal collision

--- leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
@@ -284,6 +284,9 @@ public final class LeavesConfig {
             @GlobalConfig("old-hopper-suck-in-behavior")
             public boolean oldHopperSuckInBehavior = false;
 
+            @GlobalConfig("old-nether-portal-collision") // Removed in 1.21.6
+            public boolean oldNetherPortalCollision = false;
+
             public RaidConfig raid = new RaidConfig();
 
             @GlobalConfigCategory("revert-raid-changes")

2360c4fa7f751e7810e231176c32c2cd9b0a30dd
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-05-19 19:24:11 +0800

    refactor: rename patch

--- leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
@@ -278,8 +278,8 @@ public final class LeavesConfig {
             @GlobalConfig("disable-LivingEntity-ai-step-alive-check")
             public boolean disableLivingEntityAiStepAliveCheck = false;
 
-            @GlobalConfig("use-spawn-invulnerable-time")
-            public boolean useSpawnInvulnerableTime = false;
+            @GlobalConfig("spawn-invulnerable-time")
+            public boolean spawnInvulnerableTime = false;
 
             @GlobalConfig("fix-fortress-mob-spawn")
             public boolean fixFortressMobSpawn = false;

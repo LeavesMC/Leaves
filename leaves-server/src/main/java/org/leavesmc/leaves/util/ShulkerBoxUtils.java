c0ac89b197b9ba1cf2c8c0e88b565c8248f68c1f
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-05-31 08:48:53 +0800

    Better stackable shulker

--- leaves-server/src/main/java/org/leavesmc/leaves/util/ShulkerBoxUtils.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/util/ShulkerBoxUtils.java
@@ -1,13 +1,16 @@
 package org.leavesmc.leaves.util;
 
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.item.component.ItemContainerContents;
 import net.minecraft.world.level.block.ShulkerBoxBlock;
-import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.leavesmc.leaves.LeavesConfig;
+
+import java.util.Optional;
 
 public class ShulkerBoxUtils {
 
@@ -23,10 +26,34 @@ public class ShulkerBoxUtils {
         return stack.getMaxStackSize();
     }
 
-    public static ItemStack correctItemStackMaxStackSize(ItemStack itemStack) {
-        int trulyMaxStackSize = getItemStackMaxCount(itemStack);
-        if (itemStack.getMaxStackSize() != trulyMaxStackSize) {
-            itemStack.set(DataComponents.MAX_STACK_SIZE, trulyMaxStackSize);
+    public static int getItemStackMaxCountReal(ItemStack stack) {
+        CompoundTag nbt = Optional.ofNullable(stack.get(DataComponents.CUSTOM_DATA)).orElse(CustomData.EMPTY).copyTag();
+        return nbt.getInt("leaves_real_max_stack_size").orElse(stack.getMaxStackSize());
+    }
+
+    public static ItemStack encodeMaxStackSize(ItemStack itemStack) {
+        int realMaxStackSize = getItemStackMaxCountReal(itemStack);
+        int modifiedMaxStackSize = getItemStackMaxCount(itemStack);
+        if (itemStack.getMaxStackSize() != modifiedMaxStackSize) {
+            itemStack.set(DataComponents.MAX_STACK_SIZE, modifiedMaxStackSize);
+            CompoundTag nbt = itemStack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag();
+            nbt.putInt("leaves_real_max_stack_size", realMaxStackSize);
+            itemStack.set(DataComponents.CUSTOM_DATA, CustomData.of(nbt));
+        }
+        return itemStack;
+    }
+
+    public static ItemStack decodeMaxStackSize(ItemStack itemStack) {
+        int realMaxStackSize = getItemStackMaxCountReal(itemStack);
+        if (itemStack.getMaxStackSize() != realMaxStackSize) {
+            itemStack.set(DataComponents.MAX_STACK_SIZE, realMaxStackSize);
+            CompoundTag nbt = itemStack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag();
+            nbt.remove("leaves_real_max_stack_size");
+            if (nbt.isEmpty()) {
+                itemStack.remove(DataComponents.CUSTOM_DATA);
+            } else {
+                itemStack.set(DataComponents.CUSTOM_DATA, CustomData.of(nbt));
+            }
         }
         return itemStack;
     }

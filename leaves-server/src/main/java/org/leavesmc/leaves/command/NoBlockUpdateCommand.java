855b2d87640c76c586ca899a6e3c6ee0823ab182
violetc <58360096+s-yh-china@users.noreply.github.com>
2025-06-05 17:53:18 +0800

    Remove some patch

--- leaves-server/src/main/java/org/leavesmc/leaves/command/NoBlockUpdateCommand.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/command/NoBlockUpdateCommand.java
@@ -14,6 +14,7 @@ import org.leavesmc.leaves.LeavesConfig;
 
 import java.util.List;
 
+// TODO merge to /leaves blockupdate
 public class NoBlockUpdateCommand extends Command {
 
     private static boolean noBlockUpdate = false;

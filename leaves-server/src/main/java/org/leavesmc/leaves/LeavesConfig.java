69b77759ca5534cdb0556aa271f4d61c362520a5
Helvetica Volubi <suisuroru@blue-millennium.fun>
2025-06-18 10:49:04 +0800

    refactor:fix up format

--- leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
@@ -35,6 +35,7 @@ import org.leavesmc.leaves.util.MathUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -1044,7 +1045,7 @@ public final class LeavesConfig {
         public String serverLang = "en_us";
 
         private static class ServerLangValidator extends StringConfigValidator {
-            private static final List<String> supportLang = new java.util.ArrayList<>(List.of("en_us"));
+            private static final List<String> supportLang = new ArrayList<>(List.of("en_us"));
 
             @Override
             public void verify(String old, String value) throws IllegalArgumentException {

e0342fbcb0d1448a55ac5ed91f6c83ca86f7901c
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-07-06 00:37:37 -0700

    Add use_auto to api

--- leaves-api/src/main/java/org/leavesmc/leaves/entity/botaction/BotActionType.java
+++ leaves-api/src/main/java/org/leavesmc/leaves/entity/botaction/BotActionType.java
@@ -19,9 +19,11 @@ public enum BotActionType {
     USE("use"),
     USE_ON("use_on"),
     USE_TO("use_to"),
+    USE_AUTO("use_auto"),
     USE_OFFHAND("use_offhand"),
     USE_ON_OFFHAND("use_on_offhand"),
-    USE_TO_OFFHAND("use_to_offhand");
+    USE_TO_OFFHAND("use_to_offhand"),
+    USE_AUTO_OFFHAND("use_auto_offhand");
 
     private final String name;
 

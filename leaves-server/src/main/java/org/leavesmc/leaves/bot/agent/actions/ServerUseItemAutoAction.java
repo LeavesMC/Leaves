97bdbc41d8d7f48161bf3ea9991d0f1bf522ea78
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-07-21 22:40:44 -0700

    Stupid me

--- leaves-server/src/main/java/org/leavesmc/leaves/bot/agent/actions/ServerUseItemAutoAction.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/bot/agent/actions/ServerUseItemAutoAction.java
@@ -78,7 +78,7 @@ public class ServerUseItemAutoAction extends ServerTimerBotAction<ServerUseItemA
         BlockHitResult blockHitResult = (BlockHitResult) bot.getRayTrace(5, ClipContext.Fluid.NONE);
         boolean mainSuccess, useTo = entity != null, useOn = !bot.level().getBlockState(blockHitResult.getBlockPos()).isAir();
         if (useTo) {
-            mainSuccess = ServerUseItemToAction.execute(bot, entity) || ServerUseItemAction.execute(bot);
+            mainSuccess = ServerUseItemToAction.execute(bot, entity);
         } else if (useOn) {
             mainSuccess = ServerUseItemOnAction.execute(bot, blockHitResult) || ServerUseItemAction.execute(bot);
         } else {
@@ -88,7 +88,7 @@ public class ServerUseItemAutoAction extends ServerTimerBotAction<ServerUseItemA
             return true;
         }
         if (useTo) {
-            return ServerUseItemToOffhandAction.execute(bot, entity) || ServerUseItemOffhandAction.execute(bot);
+            return ServerUseItemToOffhandAction.execute(bot, entity);
         } else if (useOn) {
             return ServerUseItemOnOffhandAction.execute(bot, blockHitResult) || ServerUseItemOffhandAction.execute(bot);
         } else {

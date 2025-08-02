0bd47b4a41de111fe4eca793c7154e205542fa60
MC_XiaoHei <xor7xiaohei@gmail.com>
2025-08-02 14:05:40 +0800

    fix: fix hopper counter config

--- leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/LeavesConfig.java
@@ -622,13 +622,15 @@ public final class LeavesConfig {
 
         public HopperCounterConfig hopperCounter = new HopperCounterConfig();
 
-        @GlobalConfigCategory("hopper-counter")
+        @GlobalConfigCategory("counter")
         public static class HopperCounterConfig {
             @RemovedConfig(name = "hopper-counter", category = "modify", transform = true)
             @RemovedConfig(name = "enabled", category = {"modify", "hopper-counter"}, transform = true)
+            @RemovedConfig(name = "enable", category = {"modify", "hopper-counter"}, transform = true)
             @GlobalConfig("enable")
             public boolean enable = false;
 
+            @RemovedConfig(name = "unlimited-speed", category = {"modify", "hopper-counter"}, transform = true)
             @GlobalConfig("unlimited-speed")
             public boolean unlimitedSpeed = false;
         }

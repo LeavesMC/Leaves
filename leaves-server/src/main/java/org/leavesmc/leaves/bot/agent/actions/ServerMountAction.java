d89a099a0dad27c27af82909e32dc1ed14942ed0
MC_XiaoHei <xor7xiaohei@gmail.com>
2025-09-24 13:37:47 +0800

    feat: use raycasting in mount action

--- leaves-server/src/main/java/org/leavesmc/leaves/bot/agent/actions/ServerMountAction.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/bot/agent/actions/ServerMountAction.java
@@ -1,8 +1,7 @@
 package org.leavesmc.leaves.bot.agent.actions;
 
-import net.minecraft.world.entity.Entity;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.entity.CraftEntity;
+import org.bukkit.craftbukkit.entity.CraftVehicle;
 import org.bukkit.entity.Vehicle;
 import org.jetbrains.annotations.NotNull;
 import org.leavesmc.leaves.bot.ServerBot;
@@ -22,14 +21,17 @@ public class ServerMountAction extends AbstractBotAction<ServerMountAction> {
         Location center = bot.getBukkitEntity().getLocation();
         List<Vehicle> vehicles = center.getNearbyEntitiesByType(
             Vehicle.class,
-            4,
-            vehicle -> manhattanDistance(bot, ((CraftEntity) vehicle).getHandle()) <= 3
+            bot.entityInteractionRange()
         ).stream().sorted(Comparator.comparingDouble(
             (vehicle) -> center.distanceSquared(vehicle.getLocation())
         )).toList();
 
         for (Vehicle vehicle : vehicles) {
-            if (bot.startRiding(((CraftEntity) vehicle).getHandle(), false)) {
+            CraftVehicle craftVehicle = (CraftVehicle) vehicle;
+            if (!bot.hasLineOfSight(craftVehicle.getHandle())) {
+                continue;
+            }
+            if (bot.startRiding(craftVehicle.getHandle(), false)) {
                 return true;
             }
         }
@@ -40,11 +42,5 @@ public class ServerMountAction extends AbstractBotAction<ServerMountAction> {
     @Override
     public Object asCraft() {
         return new CraftMountAction(this);
-    }
-
-    private double manhattanDistance(@NotNull Entity entity1, @NotNull Entity entity2) {
-        return Math.abs(entity1.getX() - entity2.getX()) +
-            Math.abs(entity1.getY() - entity2.getY()) +
-            Math.abs(entity1.getZ() - entity2.getZ());
     }
 }

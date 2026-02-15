882ab4b8d43fd7fe0ae7d568ab6e24c79f3f47b2
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-02-15 06:09:25 -0800

    New packet type and universal codec

--- leaves-server/src/main/java/org/leavesmc/leaves/bytebuf/internal/InternalBytebufHandler.java
+++ leaves-server/src/main/java/org/leavesmc/leaves/bytebuf/internal/InternalBytebufHandler.java
@@ -1,26 +1,14 @@
 package org.leavesmc.leaves.bytebuf.internal;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.collect.ImmutableMap;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.BundleDelimiterPacket;
 import net.minecraft.network.protocol.BundlePacket;
-import net.minecraft.network.protocol.common.ClientboundCustomPayloadPacket;
-import net.minecraft.network.protocol.game.ClientboundMoveEntityPacket;
-import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.Nullable;
 import org.leavesmc.leaves.LeavesConfig;
 import org.leavesmc.leaves.bytebuf.Bytebuf;
 import org.leavesmc.leaves.bytebuf.PacketAudience;
@@ -30,12 +18,10 @@ import org.leavesmc.leaves.bytebuf.PacketType;
 import org.leavesmc.leaves.event.bytebuf.PacketInEvent;
 import org.leavesmc.leaves.event.bytebuf.PacketOutEvent;
 
-import java.lang.reflect.Field;
-import java.util.List;
+import static org.leavesmc.leaves.bytebuf.internal.UniversalCodec.ID_MAP;
+import static org.leavesmc.leaves.bytebuf.internal.UniversalCodec.TYPE_MAP;
 
-import static org.leavesmc.leaves.bytebuf.PacketType.*;
-
-@SuppressWarnings({"deprecation", "rawtypes", "unchecked"})
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class InternalBytebufHandler {
 
     private static class PacketHandler extends ChannelDuplexHandler {
@@ -88,48 +74,8 @@ public class InternalBytebufHandler {
         }
     }
 
-    private static final List<String> PACKET_PACKAGES = List.of(
-        "net.minecraft.network.protocol.common",
-        "net.minecraft.network.protocol.configuration",
-        "net.minecraft.network.protocol.cookie",
-        "net.minecraft.network.protocol.game",
-        "net.minecraft.network.protocol.handshake",
-        "net.minecraft.network.protocol.login",
-        "net.minecraft.network.protocol.ping",
-        "net.minecraft.network.protocol.status"
-    );
-
     private static final SimpleBytebufAllocator ALLOCATOR = new SimpleBytebufAllocator();
-    private static final ImmutableMap<PacketType, StreamCodec> TYPE_CODEC_MAP;
-    private static final Cache<net.minecraft.network.protocol.PacketType<?>, PacketType> resultCache = CacheBuilder.newBuilder().build();
-
-    static {
-        ImmutableMap.Builder<PacketType, StreamCodec> builder = ImmutableMap.builder();
-
-        for (PacketType packet : PacketType.values()) {
-            String className = packet.name() + "Packet";
-            for (String basePackage : PACKET_PACKAGES) {
-                try {
-                    Class<?> packetClass = Class.forName(basePackage + "." + className);
-                    Field field = packetClass.getDeclaredField("STREAM_CODEC");
-                    field.setAccessible(true);
-                    builder.put(packet, (StreamCodec) field.get(null));
-                } catch (Exception ignored) {
-                }
-            }
-        }
-
-        builder.put(ClientboundMoveEntityPos, ClientboundMoveEntityPacket.Pos.STREAM_CODEC);
-        builder.put(ClientboundMoveEntityPosRot, ClientboundMoveEntityPacket.PosRot.STREAM_CODEC);
-        builder.put(ClientboundMoveEntityRot, ClientboundMoveEntityPacket.Rot.STREAM_CODEC);
-        builder.put(ServerboundMovePlayerPos, ServerboundMovePlayerPacket.Pos.STREAM_CODEC);
-        builder.put(ServerboundMovePlayerPosRot, ServerboundMovePlayerPacket.PosRot.STREAM_CODEC);
-        builder.put(ServerboundMovePlayerRot, ServerboundMovePlayerPacket.Rot.STREAM_CODEC);
-        builder.put(ServerboundMovePlayerStatusOnly, ServerboundMovePlayerPacket.StatusOnly.STREAM_CODEC);
-        builder.put(ClientboundCustomPayload, ClientboundCustomPayloadPacket.GAMEPLAY_STREAM_CODEC);
-
-        TYPE_CODEC_MAP = builder.build();
-    }
+    private static final UniversalCodec CODEC = new UniversalCodec();
 
     public static InternalBytebufHandler INSTANCE = new InternalBytebufHandler();
 
@@ -149,86 +95,36 @@ public class InternalBytebufHandler {
         if (PacketInEvent.getHandlerList().getRegisteredListeners().length == 0) {
             return nmsPacket;
         }
-        PacketType type = toEnumType(nmsPacket.type());
+        PacketType type = TYPE_MAP.get(nmsPacket.type());
         if (type == null) {
             return nmsPacket;
         }
-        WrappedBytebuf bytebuf = createWrappedBytebuf(type, nmsPacket);
+        WrappedBytebuf bytebuf = new WrappedBytebuf(CODEC.encode(nmsPacket));
         bytebuf.takeSnapshot();
         if (!new PacketInEvent(audience, type, bytebuf).callEvent()) {
             return null;
         }
-        return bytebuf.checkDirty() ? createNMSPacket(type, bytebuf) : nmsPacket;
+        return bytebuf.checkDirty() ? CODEC.decode(type, bytebuf.getRegistryBuf()) : nmsPacket;
     }
 
     public static net.minecraft.network.protocol.Packet<?> callPacketOutEvent(PacketAudience audience, net.minecraft.network.protocol.Packet<?> nmsPacket) {
         if (PacketOutEvent.getHandlerList().getRegisteredListeners().length == 0) {
             return nmsPacket;
         }
-        PacketType type = toEnumType(nmsPacket.type());
+        PacketType type = TYPE_MAP.get(nmsPacket.type());
         if (type == null) {
             return nmsPacket;
         }
-        WrappedBytebuf bytebuf = createWrappedBytebuf(type, nmsPacket);
+        WrappedBytebuf bytebuf = new WrappedBytebuf(CODEC.encode(nmsPacket));
         bytebuf.takeSnapshot();
         if (!new PacketOutEvent(audience, type, bytebuf).callEvent()) {
             return null;
         }
-        return bytebuf.checkDirty() ? createNMSPacket(type, bytebuf) : nmsPacket;
+        return bytebuf.checkDirty() ? CODEC.decode(type, bytebuf.getRegistryBuf()) : nmsPacket;
     }
 
     public static void sendPacket(PacketAudience audience, PacketType type, Bytebuf bytebuf) {
         Channel channel = (Channel) audience.getChannel();
-        channel.write(createNMSPacket(type, bytebuf));
-    }
-
-    public static net.minecraft.network.protocol.Packet<?> createNMSPacket(PacketType type, Bytebuf bytebuf) {
-        StreamCodec<FriendlyByteBuf, net.minecraft.network.protocol.Packet<?>> codec = TYPE_CODEC_MAP.get(type);
-        if (codec == null) {
-            throw new UnsupportedOperationException("This feature is not completely finished yet, packet type " + type + " is not supported temporary.");
-        }
-        return codec.decode((((WrappedBytebuf) bytebuf).getRegistryBuf()));
-    }
-
-    @Nullable
-    private static PacketType toEnumType(net.minecraft.network.protocol.PacketType<?> type) {
-        try {
-            return resultCache.get(type, () -> {
-                StringBuilder builder = new StringBuilder();
-                String bound = type.toString().split("/")[0];
-                String name = type.toString().split(":")[1];
-                builder.append(bound.substring(0, 1).toUpperCase()).append(bound.substring(1));
-                boolean flag = true;
-                for (int i = 0; i < name.length(); i++) {
-                    if (flag) {
-                        builder.append(name.substring(i, i + 1).toUpperCase());
-                        flag = false;
-                        continue;
-                    }
-                    if (name.charAt(i) == '_') {
-                        flag = true;
-                    } else {
-                        builder.append(name.charAt(i));
-                    }
-                }
-                try {
-                    return PacketType.valueOf(builder.toString());
-                } catch (IllegalArgumentException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        } catch (Exception ignore) {
-            return null;
-        }
-    }
-
-    public static WrappedBytebuf createWrappedBytebuf(PacketType type, net.minecraft.network.protocol.Packet<?> nmsPacket) {
-        RegistryFriendlyByteBuf buf = new RegistryFriendlyByteBuf(Unpooled.buffer(), MinecraftServer.getServer().registryAccess());
-        StreamCodec<FriendlyByteBuf, net.minecraft.network.protocol.Packet<?>> codec = TYPE_CODEC_MAP.get(type);
-        if (codec == null) {
-            throw new UnsupportedOperationException("This feature is not completely finished yet, packet type " + type + " is not supported temporary.");
-        }
-        codec.encode(buf, nmsPacket);
-        return new WrappedBytebuf(buf);
+        channel.write(CODEC.decode(type, ((WrappedBytebuf) bytebuf).getRegistryBuf()));
     }
 }
---
+++ leaves-server/src/main/java/org/leavesmc/leaves/bytebuf/internal/UniversalCodec.java
@@ -0,0 +1,84 @@
+package org.leavesmc.leaves.bytebuf.internal;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
+import io.netty.buffer.Unpooled;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.common.CommonPacketTypes;
+import net.minecraft.network.protocol.configuration.ConfigurationPacketTypes;
+import net.minecraft.network.protocol.cookie.CookiePacketTypes;
+import net.minecraft.network.protocol.game.GamePacketTypes;
+import net.minecraft.network.protocol.handshake.HandshakePacketTypes;
+import net.minecraft.network.protocol.login.LoginPacketTypes;
+import net.minecraft.network.protocol.ping.PingPacketTypes;
+import net.minecraft.network.protocol.status.StatusPacketTypes;
+import net.minecraft.server.MinecraftServer;
+import org.leavesmc.leaves.bytebuf.PacketType;
+
+import java.lang.reflect.Field;
+import java.util.function.Supplier;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class UniversalCodec {
+
+    static final BiMap<String, PacketType> ID_MAP;
+    static final BiMap<net.minecraft.network.protocol.PacketType<?>, PacketType> TYPE_MAP;
+    static final BiMap<PacketType, StreamCodec> CODEC_MAP;
+
+    static final Supplier<RegistryFriendlyByteBuf> BUF_FACTORY = () -> new RegistryFriendlyByteBuf(Unpooled.buffer(), MinecraftServer.getServer().registryAccess());
+
+    static {
+        ImmutableBiMap.Builder<String, PacketType> idBuilder = ImmutableBiMap.builder();
+        ImmutableBiMap.Builder<net.minecraft.network.protocol.PacketType<?>, PacketType> typeBuilder = ImmutableBiMap.builder();
+        ImmutableBiMap.Builder<PacketType, StreamCodec> codecBuilder = ImmutableBiMap.builder();
+        Class<?>[] classes = new Class<?>[]{
+            CommonPacketTypes.class, ConfigurationPacketTypes.class,
+            CookiePacketTypes.class, GamePacketTypes.class, HandshakePacketTypes.class,
+            LoginPacketTypes.class, PingPacketTypes.class, StatusPacketTypes.class
+        };
+        for (PacketType clientbound : PacketType.Clientbound.values()) {
+            idBuilder.put(clientbound.id(), clientbound);
+        }
+        for (PacketType serverbound : PacketType.Serverbound.values()) {
+            idBuilder.put(serverbound.id(), serverbound);
+        }
+        ID_MAP = idBuilder.buildOrThrow();
+        for (Class<?> clazz : classes) {
+            for (java.lang.reflect.Field field : clazz.getDeclaredFields()) {
+                try {
+                    field.setAccessible(true);
+                    net.minecraft.network.protocol.PacketType<?> packetType = (net.minecraft.network.protocol.PacketType<?>) field.get(null);
+                    Class<? extends Packet<?>> packetClass = (Class<? extends Packet<?>>) field.getGenericType().getClass();
+                    Field codecField = packetClass.getDeclaredField("STREAM_CODEC");
+                    codecField.setAccessible(true);
+                    StreamCodec codec = (StreamCodec) codecField.get(null);
+                    String id = packetType.flow().id() + "/" + packetType.id().getPath();
+                    PacketType apiType = ID_MAP.get(id);
+                    if (apiType == null) {
+                        continue;
+                    }
+                    typeBuilder.put(packetType, apiType);
+                    codecBuilder.put(apiType, codec);
+                } catch (Throwable ignored) {
+                }
+            }
+        }
+        TYPE_MAP = typeBuilder.build();
+        CODEC_MAP = codecBuilder.build();
+    }
+
+    UniversalCodec() {
+    }
+
+    public RegistryFriendlyByteBuf encode(Packet<?> packet) {
+        RegistryFriendlyByteBuf buf = BUF_FACTORY.get();
+        CODEC_MAP.get(TYPE_MAP.get(packet.type())).encode(buf, packet);
+        return buf;
+    }
+
+    public Packet<?> decode(PacketType type, RegistryFriendlyByteBuf buf) {
+        return (Packet<?>) CODEC_MAP.get(type).decode(buf);
+    }
+}
